617472fc177b9709a7cb543321010641c97c184a
Jupiter Gboyou <jupitergboyou38@gmail.com>
2026-08-13 16:31:59 +0100

    Nettoyage : suppression modules Suivi et Rapports (desactives)
    - Supprime dossiers suivi/ et rapports/ (routes + templates)
    - Retire les blueprints de app.py
    - Retire les liens commentes de base.html
    - SuiviTache conserve dans models.py (utilise par utils.py/stats)

--- rapports/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-rapports_bp = Blueprint('rapports', __name__)
-
-from rapports import routes  # noqa: E402, F401
--- suivi/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-suivi_bp = Blueprint('suivi', __name__)
-
-from suivi import routes  # noqa: E402, F401
